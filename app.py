c93cdf8e3810c8c93142aa674e5592d7281bf8b8
Poorvi <poorvi.email@example.com>
2026-05-24 19:48:59 +0530

    Fixed requirements

--- app.py
+++ app.py
@@ -874,3 +874,5 @@ def verify_face():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     app.run(debug=False) 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
